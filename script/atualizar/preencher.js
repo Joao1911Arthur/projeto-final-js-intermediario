970f7940d562459932bfc5a7038843f06bf20124
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2025-10-01 19:47:36 -0300

    .

--- script/atualizar/preencher.js
+++ script/atualizar/preencher.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let funcionario = {};
 
-
 function carregarDetalhesFuncionario() {
   var idTeste = window.location.search.substring(1);
 
@@ -12,47 +11,58 @@ function carregarDetalhesFuncionario() {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   })
-    .then(resp => resp.json())
-    .then(dados => {
+    .then((resp) => resp.json())
+    .then((dados) => {
       funcionario = dados;
 
       console.log("Funcionário carregado:", funcionario);
 
-
       if (funcionario) {
         document.getElementById("iId").textContent = `id: ${idTeste}`;
         document.getElementById("nome").value = funcionario.nome || "";
-        document.getElementById("sobrenome").value = funcionario.sobrenome || "";
-        document.getElementById("dataNascimento").value = funcionario.dtNascimento || "";
+        document.getElementById("sobrenome").value =
+          funcionario.sobrenome || "";
+        document.getElementById("dataNascimento").value =
+          funcionario.dtNascimento || "";
         document.getElementById("endereco").value = funcionario.endereco || "";
-        document.getElementById("escolaridade").value = funcionario.grauEscolaridade || "";
-        document.getElementById("salario").value = funcionario.salarioAtual || "";
-        document.getElementById("cargo").value = funcionario.cargoAtual || "";
+        document.getElementById("escolaridade").value =
+          funcionario.grauEscolaridade || "";
+        document.getElementById("salario").value =
+          funcionario.salarioAtual || "";
+        // mudou para pegar o novo formato de cargos e Salarios. nome estava errado
+        document.getElementById("cargo").value =
+          funcionario.historicoCargosESalarios[0].cargo || "";
         document.getElementById("sexo").value = funcionario.sexo || "";
         document.getElementById("foto").src = "";
-
 
         const valorPassagem = document.getElementById("valorPassagem");
         valorPassagem.value = funcionario.valorPassagem || "";
         valorPassagem.disabled = !funcionario.optouVT;
 
         if (funcionario.optouVT) {
-          document.querySelector('input[name="vt"][value="sim"]').checked = true;
+          document.querySelector(
+            'input[name="vt"][value="sim"]'
+          ).checked = true;
         } else {
-          document.querySelector('input[name="vt"][value="nao"]').checked = true;
+          document.querySelector(
+            'input[name="vt"][value="nao"]'
+          ).checked = true;
         }
 
-        document.querySelectorAll('input[name="vt"]').forEach(radio => {
+        document.querySelectorAll('input[name="vt"]').forEach((radio) => {
           radio.addEventListener("change", () => {
             if (radio.value === "sim") {
               valorPassagem.disabled = false;
+              // mudou para receber o valor cadastrado da passagem caso tenha optado por VT
+              valorPassagem.value = funcionario.valorPassagem;
             } else {
+              // mudou para receber o valor 0 caso nao tenha optado por VT
               valorPassagem.disabled = true;
-              valorPassagem.value = "";
+              valorPassagem.value = 0;
             }
           });
         });
       }
     })
-    .catch(err => console.error("Erro na requisição:", err));
+    .catch((err) => console.error("Erro na requisição:", err));
 }
